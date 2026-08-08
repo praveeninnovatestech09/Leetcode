6af220e319d425e66f06660959dc7b031cfadd07
praveeninnovatestech09 <praveeninnovates.tech@gmail.com>
2026-08-08 21:55:12 +0530

    Time: 72 ms (59.7%), Space: 193.3 MB (84.39%) - LeetHub

--- 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.cpp
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.cpp
@@ -1,74 +1,47 @@
 class Solution {
 public:
     int sumSubarrayMins(vector<int>& arr) {
-
-        int n = arr.size();
-
-        int MOD = 1e9 + 7;
-
-        // Stores number of choices on left
-        vector<int> left(n);
-
-        // Stores number of choices on right
-        vector<int> right(n);
-
-        stack<int> st;
-
-        // ---------- Previous Smaller ----------
-        for(int i = 0; i < n; i++) {
-
-            // Remove all greater elements
-            while(!st.empty() && arr[st.top()] > arr[i]) {
+        int n =arr.size();
+        const int MOD=1e9+7;
+        vector<int>left(n);
+        vector<int>right(n);
+        stack<int>st;
+        //for pse
+        for(int i =0; i<n;i++){
+            while(!st.empty()&& arr[st.top()]>arr[i]){
                 st.pop();
             }
-
-            // No previous smaller
-            if(st.empty())
-                left[i] = i + 1;
-
-            // Distance from previous smaller
-            else
-                left[i] = i - st.top();
-
-            // Store current index
+            if(st.empty()){
+                left[i]= i+1;
+            }
+            else{
+                left[i]= i-st.top();
+            }
             st.push(i);
         }
-
-        // Empty stack
-        while(!st.empty())
+        //emptystack
+        while(!st.empty()){
             st.pop();
-
-        // ---------- Next Smaller or Equal ----------
-        for(int i = n - 1; i >= 0; i--) {
-
-            // Remove greater or equal elements
-            while(!st.empty() && arr[st.top()] >= arr[i]) {
+        }
+        // for nse
+        for(int i = n-1; i>=0;i--){
+            while(!st.empty()&& arr[st.top()]>=arr[i]){
                 st.pop();
             }
-
-            // No next smaller
-            if(st.empty())
-                right[i] = n - i;
-
-            // Distance from next smaller
-            else
-                right[i] = st.top() - i;
-
-            // Store current index
+            if(st.empty()){
+                right[i]= n-i;
+            }
+            else{
+                right[i]=st.top()-i;
+            }
             st.push(i);
         }
 
-        long long ans = 0;
-
-        // Calculate contribution of every element
-        for(int i = 0; i < n; i++) {
-
-            long long contribution =
-                (1LL * arr[i] * left[i] * right[i]) % MOD;
-
-            ans = (ans + contribution) % MOD;
+        long long ans=0;
+        for(int i=0;i<n;i++){
+            long long contro = 1LL*left[i]*right[i]*arr[i];
+            ans= (ans+contro)%MOD;
         }
-
         return ans;
     }
 };
