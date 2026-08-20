c08123faa93f0b8154ec7fbb72117c93b2756a85
praveeninnovatestech09 <praveeninnovates.tech@gmail.com>
2026-08-20 12:33:13 +0530

    Time: 48 ms (37.76%), Space: 84.8 MB (84.22%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
@@ -8,7 +8,7 @@ public:
         unordered_map<int,int>freq;
         while(r<fruits.size()){
             freq[fruits[r]]++;
-            if(freq.size()>2){
+            while(freq.size()>2){
                 freq[fruits[l]]--;
                 if(freq[fruits[l]]==0) freq.erase(fruits[l]);
                 l++;
